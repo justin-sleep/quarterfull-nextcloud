2efbb0a861b4aebe9176d143aad8fc130ced6a9b
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-11-19 16:33:19 +0100

    replace session implementation if it changes at runtime

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -990,7 +990,8 @@ class Server extends ServerContainer implements IServerContainer {
 	 */
 	public function setSession(\OCP\ISession $session) {
 		$this->query(SessionStorage::class)->setSession($session);
-		return $this->query('UserSession')->setSession($session);
+		$this->query('UserSession')->setSession($session);
+		$this->query(Store::class)->setSession($session);
 	}
 
 	/**
